8d241b23341e23da558b1470eaa872da58523d40
nagolli <nacho.gomis@gmail.com>
2026-05-06 22:56:55 +0200

    Instrucciones PlayerProfile

--- Buscaminas/app/views/playerProfile.tsx
+++ Buscaminas/app/views/playerProfile.tsx
@@ -3,13 +3,13 @@ export interface PlayerProfileProps {
 }
 
 export default function PlayerProfile({ }: PlayerProfileProps) {
-    // TODO: Implementar botón para ir a nueva partida
+    // TODO: Implementar botón para ir a nueva partida (navigateToView(Views.SelectMode))
 
-    // TODO: Implementar desplegable con cada perfil guardado (con nombre)
+    // TODO: Implementar desplegable con cada perfil guardado (con nombre), el seleccionado se marca como activo en BBDD y se muestra su historial de partidas
 
     // TODO: Implementar botón para crear nuevo perfil (con input para nombre)
 
-    // TODO: Implementar lista de partidas guardadas para el perfil seleccionado (fecha, configuración, resultado)
+    // TODO: Implementar lista de partidas guardadas para el perfil seleccionado (fecha, configuración, resultado, ...)
 
     return <>
 
